9289ab3be9d82884583ea5eb3a5f5dfdf93e9d19
Ana <ana.godoy@ccc.ufcg.edu.br>
2018-09-13 00:29:09 -0300

    Adding heroku db

--- config/database.js
+++ config/database.js
@@ -1,23 +1,26 @@
 module.exports = {
   'development': {
-    'username': 'root',
-    'password': '',
-    'database': 'test',
-    'host': '127.0.0.1',
+    'username': 'jpzmmfnqestolx',
+    'password': '3ea1a73b09f541dae9e8edc062663612e4c1e6d7c355552ba3807c213c5aace4',
+    'database': 'da49hvhhm66phr',
+    'host': 'ec2-107-21-233-72.compute-1.amazonaws.com',
+    'port': '5432',
     'dialect': 'mysql'
   },
   'test': {
-    'username': 'root',
-    'password': '',
-    'database': 'database_test',
-    'host': '127.0.0.1',
+    'username': 'jpzmmfnqestolx',
+    'password': '3ea1a73b09f541dae9e8edc062663612e4c1e6d7c355552ba3807c213c5aace4',
+    'database': 'da49hvhhm66phr',
+    'host': '127.ec2-107-21-233-72.compute-1.amazonaws.com',
+    'port': '5432',
     'dialect': 'mysql'
   },
   'production': {
-    'username': 'root',
-    'password': '',
-    'database': 'database_production',
-    'host': '127.0.0.1',
+    'username': 'jpzmmfnqestolx',
+    'password': '3ea1a73b09f541dae9e8edc062663612e4c1e6d7c355552ba3807c213c5aace4',
+    'database': 'da49hvhhm66phr',
+    'host': 'ec2-107-21-233-72.compute-1.amazonaws.com',
+    'port': '5432',
     'dialect': 'mysql'
   }
 };
