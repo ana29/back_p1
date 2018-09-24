460ffce3c9f797d0b43b8b6b25c5494c933c742f
Ana <ana.godoy@ccc.ufcg.edu.br>
2018-09-24 14:38:26 -0300

    Adding password encryption

--- app/models/condominium.js
+++ app/models/condominium.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcrypt');
+
 module.exports = (sequelize, DataTypes) => {
     const Condominiums = sequelize.define('Condominiums', {
         name: DataTypes.STRING,
@@ -8,6 +10,23 @@ module.exports = (sequelize, DataTypes) => {
         cpf_admin: DataTypes.STRING,
         email_admin: DataTypes.STRING,
     });
+
+    Condominiums.beforeCreate((condominiums) => {
+        return bcrypt.hash(condominiums.password, process.env.BCRYPT_SALT_ROUNDS || 10)
+            .then((hash) => {
+                condominiums.password = hash;
+            });
+    });
+
+    Condominiums.beforeBulkUpdate((condominiums) => {
+        if (condominiums.attributes.password) {
+            return bcrypt.hash(condominiums.attributes.password, process.env.BCRYPT_SALT_ROUNDS || 10)
+                .then((hash) => {
+                    condominiums.attributes.password = hash;
+                });
+        }
+    });
+
     return Condominiums;
 }
 
--- app/models/resident.js
+++ app/models/resident.js
@@ -12,6 +12,24 @@ module.exports = (sequelize, DataTypes) => {
         phone: DataTypes.STRING,
         condominium_cnpj: DataTypes.STRING,
     });
+    Residents.beforeCreate((resident) => {
+        console.log(resident);
+
+        return bcrypt.hash(resident.password, process.env.BCRYPT_SALT_ROUNDS || 10)
+            .then((hash) => {
+                resident.password = hash;
+            });
+    });
+
+    Residents.beforeBulkUpdate((resident) => {
+        console.log(resident.password);
+        if (resident.attributes.password) {
+            return bcrypt.hash(resident.attributes.password, process.env.BCRYPT_SALT_ROUNDS || 10)
+                .then((hash) => {
+                    resident.attributes.password = hash;
+                });
+        }
+    });
 
     return Residents;
-}
+};
