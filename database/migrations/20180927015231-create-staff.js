c05215d6f9788027d4b7f6512889df8383324450
ana <godoyisadora@gmail.com>
2018-09-27 09:42:28 -0300

    Updating migrating

--- database/migrations/20180927015231-create-staff.js
+++ database/migrations/20180927015231-create-staff.js
@@ -1,23 +1,56 @@
-'use strict';
-
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
+    up: (queryInterface, DataTypes) => {
+        queryInterface.createTable('Staff', {
 
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
-  },
 
-  down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: DataTypes.INTEGER,
+            },
+            name: {
+                allowNull: false,
+                type: DataTypes.STRING,
+            },
+            cpf: {
+                allowNull: false,
+                type: DataTypes.STRING,
+                unique: true,
+            },
+            email: {
+                allowNull: false,
+                type: DataTypes.STRING,
+                unique: true,
+            },
+            password: {
+                allowNull: false,
+                type: DataTypes.STRING,
+            },
+            phone: {
+                allowNull: false,
+                type: DataTypes.STRING,
+            },
+            condominium_cnpj: {
+                allowNull: false,
+                type: DataTypes.STRING,
+            },
+            permission: {
+                allowNull: false,
+                type: DataTypes.INTEGER,
+            },
+            createdAt: {
+                allowNull: false,
+                type: DataTypes.DATE,
+            },
+            updatedAt: {
+                allowNull: false,
+                type: DataTypes.DATE,
+            },
+        });
+    },
 
-      Example:
-      return queryInterface.dropTable('users');
-    */
-  }
+    down: (queryInterface) => {
+        queryInterface.dropTable('Staff');
+    }
 };
