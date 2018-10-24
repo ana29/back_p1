2bc94f3350baed5ca67f6712b1a362e1f1858b82
Ana <ana.godoy@ccc.ufcg.edu.br>
2018-10-24 00:41:03 -0300

    Updating core

--- app/core/jsonWebToken.js
+++ app/core/jsonWebToken.js
@@ -3,28 +3,28 @@ const config = require('config');
 const HttpStatusCodes = require('http-status-codes');
 
 module.exports = {
-  generateToken: (id) => {
-    return jwt.sign({ id }, config.get('jwtSecret'), { expiresIn: '7d' });
-  },
+    generateToken: (id) => {
+        return jwt.sign({id}, config.get('jwtSecret'), {expiresIn: '7d'});
+    },
 
-  /**
-   * To use JWT authentication implemented by this core, you must implement a global method called findUserById, 
-   * which returns JSON in the form {date: {your: data}}
-   */
-  authenticate: async (req, res, next) => {
-    try {
-      const token = req.get('Authorization') || req.get('authorization');
-      let decoded = jwt.verify(token, config.get('jwtSecret'));
-      let user = await global.findUserById(decoded.id);
-      if (user) {
-        req.user = user.data;
-        next();
-      } else {
-        return res.status(HttpStatusCodes.UNAUTHORIZED).send();
-      }
-    } catch(err) {
-      return res.status(HttpStatusCodes.UNAUTHORIZED).send();
+    /**
+     * To use JWT authentication implemented by this core, you must implement a global method called findUserById,
+     * which returns JSON in the form {date: {your: data}}
+     */
+    authenticate: async (req, res, next) => {
+        try {
+            const token = req.get('Authorization') || req.get('authorization');
+            let decoded = jwt.verify(token, config.get('jwtSecret'));
+            let user = await global.findUserById(decoded.id);
+            if (user) {
+                req.user = user.data;
+                next();
+            } else {
+                return res.status(HttpStatusCodes.UNAUTHORIZED).send();
+            }
+        } catch (err) {
+            return res.status(HttpStatusCodes.UNAUTHORIZED).send();
+        }
     }
-  }
 
 };
--- app/core/routes.js
+++ app/core/routes.js
@@ -1,18 +1,18 @@
-const glob      = require('glob');
-const _         = require('lodash');
-const path      = require('path');
-const express   = require('express');
+const glob = require('glob');
+const _ = require('lodash');
+const path = require('path');
+const express = require('express');
 const pluralize = require('pluralize');
 
 module.exports = (app) => {
-  const routes = glob.sync(__dirname + '/../services/**/*route.js', {});
+    const routes = glob.sync(__dirname + '/../services/**/*route.js', {});
 
     _.each(routes, (file) => {
-    let routeName = path.basename(file, '.route.js');
-    let router = express.Router(routeName);
+        let routeName = path.basename(file, '.route.js');
+        let router = express.Router(routeName);
 
-    console.log('/' + pluralize(routeName));
-    require(file)(router);
-    app.use('/' + pluralize(routeName), router);
+        console.log('/' + pluralize(routeName));
+        require(file)(router);
+        app.use('/' + pluralize(routeName), router);
     });
 };
