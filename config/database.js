d46071d0d6dd488860c1ce7730bb08eda4465426
ana29 <godoyisadora@gmail.com>
2018-11-12 14:47:46 -0300

    adding lodash

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
     'development': {
         'username': 'root',
-        'password': '',
+        'password': '123456',
         'database': 'test',
         'host': '127.0.0.1',
         'dialect': 'mysql'
