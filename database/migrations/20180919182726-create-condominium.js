b272352e533ea056f9c77df6541ed58666074658
Ana <ana.godoy@ccc.ufcg.edu.br>
2018-11-29 14:32:29 -0300

    tst

--- database/migrations/20180919182726-create-condominium.js
+++ database/migrations/20180919182726-create-condominium.js
@@ -7,6 +7,7 @@ module.exports = {
                 allowNull: false,
                 autoIncrement: true,
                 primaryKey: true,
+                unique: true,
                 type: DataTypes.INTEGER
             },
             name: {
