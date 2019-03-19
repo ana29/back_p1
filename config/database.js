f77e83989f6dde420d2de1709d992fd978eacd0d
Ana <ana.godoy@ccc.ufcg.edu.br>
2019-03-18 23:34:53 -0300

    update

--- config/database.js
+++ config/database.js
@@ -1,6 +1,4 @@
 require('dotenv').config();
-const Sequelize = require("sequelize");
-
 
 module.exports = {
     'development': {
@@ -23,7 +21,6 @@ module.exports = {
         'password': '3ea1a73b09f541dae9e8edc062663612e4c1e6d7c355552ba3807c213c5aace4',
         'database': 'da49hvhhm66phr',
         'host': 'ec2-107-21-233-72.compute-1.amazonaws.com',
-        'operatorsAliases' : Sequelize.Op,
         'dialect': 'postgres'
     }
 };
