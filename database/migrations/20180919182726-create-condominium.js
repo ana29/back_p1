1ecea85271d2663d38db270a6a63d3df433dabb4
Ana <ana.godoy@ccc.ufcg.edu.br>
2019-03-18 23:16:59 -0300

    update

--- database/migrations/20180919182726-create-condominium.js
+++ database/migrations/20180919182726-create-condominium.js
@@ -1,48 +1,48 @@
-// 'use strict';
-//
-// module.exports = {
-//     up: (queryInterface, DataTypes) => {
-//         queryInterface.createTable('Condominiums', {
-//             id: {
-//                 allowNull: false,
-//                 autoIncrement: true,
-//                 primaryKey: true,
-//                 type: DataTypes.INTEGER
-//             },
-//             name: {
-//                 allowNull: false,
-//                 type: DataTypes.STRING
-//             },
-//             cnpj: {
-//                 allowNull: false,
-//                 unique: true,
-//                 type: DataTypes.STRING
-//             },
-//             phone: {
-//                 allowNull: false,
-//                 type: DataTypes.STRING
-//             },
-//             address: {
-//                 allowNull: false,
-//                 type: DataTypes.STRING
-//             },
-//             createdAt: {
-//                 allowNull: false,
-//                 type: DataTypes.DATE
-//             },
-//             updatedAt: {
-//                 allowNull: false,
-//                 type: DataTypes.DATE
-//             }
-//         });
-//     },
-//
-//     down: (queryInterface) => {
-//         queryInterface.dropTable('Condominiums');
-//     }
-// };
-//
-//
-//
-//
-//
+'use strict';
+
+module.exports = {
+    up: (queryInterface, DataTypes) => {
+        queryInterface.createTable('Condominiums', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: DataTypes.INTEGER
+            },
+            // name: {
+            //     allowNull: false,
+            //     type: DataTypes.STRING
+            // },
+            // cnpj: {
+            //     allowNull: false,
+            //     unique: true,
+            //     type: DataTypes.STRING
+            // },
+            // phone: {
+            //     allowNull: false,
+            //     type: DataTypes.STRING
+            // },
+            // address: {
+            //     allowNull: false,
+            //     type: DataTypes.STRING
+            // },
+            // createdAt: {
+            //     allowNull: false,
+            //     type: DataTypes.DATE
+            // },
+            // updatedAt: {
+            //     allowNull: false,
+            //     type: DataTypes.DATE
+            // }
+        });
+    },
+
+    down: (queryInterface) => {
+        queryInterface.dropTable('Condominiums');
+    }
+};
+
+
+
+
+
